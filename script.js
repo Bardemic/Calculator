e65e1ac310f297d8cb4a34b24be18c911c860160
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-07-05 07:21:06 -0500

    Add evaluate function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let add = ((num1, num2) => num1 + num2);
+let num1;
+let num2;
+let operator;
 
-let subtract = ((num1, num2) => num1 - num2);
+const add = ((num1, num2) => num1 + num2);
 
-let multiply = ((num1, num2) => num1 * num2);
+const subtract = ((num1, num2) => num1 - num2);
 
-let divide = ((num1, num2) => num1 / num2);
+const multiply = ((num1, num2) => num1 * num2);
+
+const divide = ((num1, num2) => num1 / num2);
+
+function evaluate(num1, num2, operator){
+    if(operator === "+") return add(num1, num2);
+    if(operator === "-") return subtract(num1, num2);
+    if(operator === "*") return multiply(num1, num2);
+    if(operator === "/") return divide(num1, num2);
+}
