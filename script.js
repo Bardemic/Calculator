9da50fd8d2483c939e1d3f8a3033a31520502c6b
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-07-07 23:59:59 -0500

    Code changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let num1 = 0;
 let num2 = 0;
-let operator;
+let operator = "+";
 let displayValue;
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
@@ -20,38 +20,41 @@ function operate(num1, num2, operator){
     if(operator === "/") return divide(num1, num2);
 };
 
-function updateDisplay(number) {
-    console.log("Num1: " + num1 + " Num2: " + num2);
-    display.innerHTML += number;
+function updateDisplay(input) {
+    console.log(input);
+    if(input.classList.contains("operator")){
+        display.innerHTML = num1;
+    }
+    else if(input.classList.contains("number")){
+        display.innerHTML += input.innerHTML;
+    }
 }
 
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
-        console.log(e.target);
+        //console.log(e.target);
         pressedButton(e.target);
     })
 })
 
 function pressedButton(button) {
     if(button.classList.contains("number")){
-        updateDisplay(button.innerHTML);
+        updateDisplay(button);
         num2 = parseFloat(num2 + button.innerHTML);
-        console.log(num2);
-        console.log(button.innerHTML);
     }
     else if(button.id === "."){
-        updateDisplay(button.innerHTML);
+        updateDisplay(button);
         num2 += ".";
     }
     else if(num1 === 0){
         num1 = num2;
         num2 = 0;
-        updateDisplay(button.innerHTML);
+        updateDisplay(button);
         operator = button.id;
     }
     else if(button.id === "+"){
-        updateDisplay(button.innerHTML);
+        updateDisplay(button);
         operator = "+";
         num1 = operate(num1, num2, operator);
         num2 = 0;
@@ -62,22 +65,22 @@ function pressedButton(button) {
         num2 = 0;
     }
     else if(button.id === "-"){
-        updateDisplay(button.innerHTML);
+        updateDisplay(button);
         operator = "-";
         num1 = operate(num1, num2, operator);
         num2 = 0;
     }
     else if(button.id === "/"){
-        updateDisplay(button.innerHTML);
+        updateDisplay(button);
         operator = "/";
         num1 = operate(num1, num2, operator);
         num2 = 0;
     }
     else if(button.id === "*"){
-        updateDisplay(button.innerHTML);
         operator = "*";
         num1 = operate(num1, num2, operator);
         num2 = 0;
+        updateDisplay(button);
     }
 }
 
